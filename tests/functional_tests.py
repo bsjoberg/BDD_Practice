33b8187f5dff2ddd5217aa25e3a7f29a6b8bb746
Brian <brian.sjoberg@excella.com>
2021-10-12 20:30:20 -0400

    Added a more specific assertion for django.

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -3,4 +3,5 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
-assert 'install' in browser.title
+assert 'install worked' in browser.title
+assert 'django' in browser.page_source
