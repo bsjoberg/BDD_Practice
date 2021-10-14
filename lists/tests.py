421bddd2491fa2f2ba342be899aa3ec156f34a5b
Brian <brian.sjoberg@excella.com>
2021-10-14 14:00:09 -0400

    Still need to get foreignkey working in model.

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.http import HttpRequest
 from django.urls import resolve
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 
@@ -15,13 +15,21 @@ class HomePageTest(TestCase):
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -29,7 +37,9 @@ class ItemModelTest(TestCase):
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
