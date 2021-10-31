2e8ac5ecb13fc8406772ab9b37f951b5e7f74779
brian <brian.sjoberg@excella.com>
2021-10-31 09:02:38 -0400

    Added behave environment file to ensure browser quits properly.

--- features/todo_website.feature
+++ features/todo_website.feature
@@ -2,4 +2,4 @@ Feature: Is my website available
   Scenario: Test if django is running
     Given I have django running
     When I go to the website
-    Then I see "To-Do" in the title
+    Then I see "install worked" in the title
--- steps/todo_website_steps.py
+++ steps/todo_website_steps.py
@@ -1,11 +1,14 @@
 from behave import given, when, then
 from selenium import webdriver
-import unittest
+
+
+def after_all(context):
+    print("after")
 
 
 @given(u'I have django running')
 def step_impl(context):
-    context.result = webdriver.Firefox()
+    context.result = context.browser
 
 
 @when(u'I go to the website')
@@ -15,5 +18,5 @@ def step_impl(context):
 
 @then(u'I see "{expected_result}" in the title')
 def step_impl(context, expected_result):
-    assert expected_result in context.result.title
+    assert expected_result in context.result.title, f"Expected {expected_result} but got {context.result.title}"
     context.result.quit()
