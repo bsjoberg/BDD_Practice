12515c1caa92395bcb61154ca2d2dec4c34f5bd8
brian <brian.sjoberg@excella.com>
2021-10-31 12:07:21 -0400

    Got the unit test working with some minimal html code added to views. Functional test still failing but it is a reminder.

--- environment.py
+++ environment.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 
 def before_all(context):
     context.browser = webdriver.Firefox()
-    # context.browser.set_page_load_timeout(10)
-    # context.browser.implicitly_wait(10)
+    context.browser.set_page_load_timeout(10)
+    context.browser.implicitly_wait(10)
 
 
 def after_all(context):
