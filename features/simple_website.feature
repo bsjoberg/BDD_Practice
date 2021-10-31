0260c3467557488bd1672642b5dcf454d5cd4757
brian <brian.sjoberg@excella.com>
2021-10-30 22:06:12 -0400

    Created a behave version of the initial functional test for testing if django is installed.

--- features/simple_website.feature
+++ features/simple_website.feature
@@ -2,4 +2,4 @@ Feature: Is my website available
   Scenario: Test if django is running
     Given I have django running
     When I go to the website
-    Then I see "Django" in the title
+    Then I see "The install worked successfully" in the title
---
+++ steps/simple_website_steps.py
@@ -0,0 +1,17 @@
+from behave import given, when, then
+from selenium import webdriver
+
+
+@given(u'I have django running')
+def step_impl(context):
+    context.result = webdriver.Firefox()
+
+
+@when(u'I go to the website')
+def step_impl(context):
+    context.result.get('http://localhost:8000')
+
+
+@then(u'I see "{expected_result}" in the title')
+def step_impl(context, expected_result):
+    assert expected_result in context.result.title
